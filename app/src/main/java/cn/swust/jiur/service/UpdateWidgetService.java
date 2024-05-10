800d206451caaad3b17405784fdd1cbc06650e13
JiuR <2041357138@qq.com>
2024-05-10 20:01:42 +0800

    refresh

---
+++ app/src/main/java/cn/swust/jiur/service/UpdateWidgetService.java
@@ -0,0 +1,16 @@
+package cn.swust.jiur.service;
+
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.JobIntentService;
+
+public class UpdateWidgetService extends JobIntentService {
+
+    @Override
+    protected void onHandleWork(@NonNull Intent intent) {
+
+    }
+}
